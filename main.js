e3f5d94623b6a183fd3923330e5a77c6f7fab4b0
javohirhasano <ijavoxir5@gmail.com>
2021-10-24 14:04:42 +0500

    ghjk

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ window.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('scroll', function () {
         const header = document.querySelector('header')
         header.classList.toggle('sticky', window.scrollY > 0)
-
+    })
+    const bars = document.querySelector('#bars')
+    bars.addEventListener('click', function () {
+        bars.classList.toggle('active')
+        bars.classList.toggle('fa-times')
+        bars.classList.toggle('header_menu')
     })
 })
